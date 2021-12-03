52160f6d97efcf981a8cbb63606e89ac5867f409
mdb1c15 <mattia.dalborgo@gmail.com>
2021-12-03 10:53:29 +0100

    read csv for lpm_update

--- dtApp/dtCode/lpm_update.py
+++ dtApp/dtCode/lpm_update.py
@@ -4,7 +4,11 @@ This function updates the parameters of the 3dof model.
 from flask import render_template
 from dtApp import app
 from dtApp import date
+from numpy import genfromtxt
 
-@app.route('/lpm_update')
+
+@app.route('/lpm_update', methods=['GET','POST'])
 def lpm_update():
+    frf_h11 = genfromtxt('dtData/test_1.csv', delimiter=',')
+    
     return render_template('lpm_update.html',date=date)
