e700d5dbb5da540d03168bed2bf02685f5e25e1c
Marco <marcodeangelis09@gmail.com>
2022-04-26 11:30:10 +0100

    more readability, values no longer hard coded in variables namespace.

--- dtLib/crossval/data_location.py
+++ dtLib/crossval/data_location.py
@@ -1,34 +1,44 @@
-data_location = {
+data_particulars = {
     'experimental':{
         'Swansea':{
             'fullpath':'dtLib/crossval/Data/Experimental Data/ExpDataSw.csv',
+            'slice_range':[0,322],
             },
         'Sheffield':{
             'fullpath':'dtLib/crossval/Data/Experimental Data/ExpDataSh.csv',
+            'slice_range':[0,6561],
             },
         'Southampton':{
             'fullpath':'dtLib/crossval/Data/Experimental Data/ExpDataSo.csv',
+            'slice_range':[0,1313],
             },
         'Bristol':{
             'fullpath':'dtLib/crossval/Data/Experimental Data/ExpDataBr.csv',
+            'slice_range':[0,80002],
             },
     },
     'simulated':{
         'Swansea':{
             'fullpath':'dtLib/crossval/Data/Numerical Data/NumDataSw.csv',
+            'slice_range':[0,3998],
             },
         'Sheffield':{
             'fullpath':'dtLib/crossval/Data/Numerical Data/NumDataSh.csv',
+            'slice_range':[0,3998],
             },
         'Southampton':{
             'fullpath':'dtLib/crossval/Data/Numerical Data/NumDataSo.csv',
+            'slice_range':[0,3992],
             },
         'Bristol':{
             'fullpath':'dtLib/crossval/Data/Numerical Data/NumDataBr.csv',
+            'slice_range':[0,400],
             },
     }
 }
 
+# LimitsExp = np.array([0,322,6561,1313,80002]) #0/Sw/Sh/So/Br
+# LimitsNum = np.array([0,3998,3998,3992,400]) #0/Sw/Sh/So/Br
 
 # dataSwExp = readfile_gen('dtLib/crossval/Data/Experimental Data/ExpDataSw.csv')
 # dataShExp = readfile_gen('dtLib/crossval/Data/Experimental Data/ExpDataSh.csv')
--- dtLib/crossval/extract_data.py
+++ dtLib/crossval/extract_data.py
@@ -4,7 +4,7 @@ This function extracts the experimental and simulated data.
 import numpy as np
 from typing import Generator
 
-from .data_location import data_location
+from .data_location import data_particulars
 
 def readfile_gen(fullpath,sep=','):
     with open(fullpath,"r") as f:
@@ -13,56 +13,74 @@ def readfile_gen(fullpath,sep=','):
 def slice_data(x:Generator,istart:int,iend:int):
     x_=iter(x)
     X=[]
+    X_append = X.append
     i=0
     while True:
         try: xi = next(x_)
-        except: break
-        if (istart<=i) & (i<=iend): X.append(xi)
+        except StopIteration: break
+        if (istart<=i) & (i<=iend): X_append(xi)
         i+=1
     return np.asarray(X,dtype=float)
 
 def extract_data(fullpath,istart:int=0,iend:int=10_000): 
     return slice_data(readfile_gen(fullpath),istart=istart,iend=iend)
 
-LimitsExp = np.array([0,322,6561,1313,80002]) #0/Sw/Sh/So/Br
-LimitsNum = np.array([0,3998,3998,3992,400]) #0/Sw/Sh/So/Br
+# LimitsExp = np.array([0,322,6561,1313,80002]) #0/Sw/Sh/So/Br
+# LimitsNum = np.array([0,3998,3998,3992,400]) #0/Sw/Sh/So/Br
 
-dataSwExp = extract_data(data_location['experimental']['Swansea']['fullpath'],istart=LimitsExp[0],iend=LimitsExp[1])
-tSW = dataSwExp[:,0]
-youtSW = dataSwExp[:,1:4]
+################################ Experimental Data ######################
+limits_sw_exp = data_particulars['experimental']['Swansea']['slice_range']
+path_sw_exp = data_particulars['experimental']['Swansea']['fullpath']
+data_sw_exp = extract_data(path_sw_exp,istart=limits_sw_exp[0],iend=limits_sw_exp[1]) # dataSwExp = extract_data(data_particulars['experimental']['Swansea']['fullpath'],istart=LimitsExp[0],iend=LimitsExp[1])
+tSW = data_sw_exp[:,0] # KEEP VARIABLE NAME
+youtSW = data_sw_exp[:,1:4] # KEEP VARIABLE NAME
 
-dataShExp = extract_data(data_location['experimental']['Sheffield']['fullpath'],istart=LimitsExp[0],iend=LimitsExp[2])
-tSH = dataShExp[:,0]
-youtSH = dataShExp[:,1:4]
+limits_sh_exp = data_particulars['experimental']['Sheffield']['slice_range']
+path_sh_exp = data_particulars['experimental']['Sheffield']['fullpath']
+data_sh_exp = extract_data(path_sh_exp,istart=limits_sh_exp[0],iend=limits_sh_exp[1]) # dataShExp = extract_data(data_particulars['experimental']['Sheffield']['fullpath'],istart=LimitsExp[0],iend=LimitsExp[2])
+tSH = data_sh_exp[:,0] # KEEP VARIABLE NAME
+youtSH = data_sh_exp[:,1:4] # KEEP VARIABLE NAME
 
-dataSoExp = extract_data(data_location['experimental']['Southampton']['fullpath'],istart=LimitsExp[0],iend=LimitsExp[3])
-tSO = dataSoExp[:,0]
-youtSO = dataSoExp[:,1:4]
+limits_so_exp = data_particulars['experimental']['Southampton']['slice_range']
+path_so_exp = data_particulars['experimental']['Southampton']['fullpath']
+data_so_exp = extract_data(path_so_exp,istart=limits_so_exp[0],iend=limits_so_exp[1]) # dataSoExp = extract_data(data_particulars['experimental']['Southampton']['fullpath'],istart=LimitsExp[0],iend=LimitsExp[3])
+tSO = data_so_exp[:,0] # KEEP VARIABLE NAME
+youtSO = data_so_exp[:,1:4] # KEEP VARIABLE NAME
 
-dataBrExp = extract_data(data_location['experimental']['Bristol']['fullpath'],istart=LimitsExp[0],iend=LimitsExp[4])
-tBR = dataBrExp[:,0]
-youtBR = dataBrExp[:,1:4]
+limits_br_exp = data_particulars['experimental']['Bristol']['slice_range']
+path_br_exp = data_particulars['experimental']['Bristol']['fullpath']
+data_br_exp = extract_data(path_br_exp,istart=limits_br_exp[0],iend=limits_br_exp[1]) # dataBrExp = extract_data(data_particulars['experimental']['Bristol']['fullpath'],istart=LimitsExp[0],iend=LimitsExp[4])
+tBR = data_br_exp[:,0] # KEEP VARIABLE NAME
+youtBR = data_br_exp[:,1:4] # KEEP VARIABLE NAME
 
+################################ Simulated Data ######################
+limits_sw_num = data_particulars['simulated']['Swansea']['slice_range']
+path_sw_num = data_particulars['simulated']['Swansea']['fullpath']
+data_sw_num = extract_data(path_sw_num,istart=limits_sw_num[0],iend=limits_sw_num[1]) # dataSwNum = extract_data(data_particulars['simulated']['Swansea']['fullpath'],istart=LimitsNum[0],iend=LimitsNum[1])
+tNumSW = data_sw_num[:,0] # KEEP VARIABLE NAME
+tNumSWmiddle = data_sw_num[:,1] 
+youtNumSW = data_sw_num[:,2:] # KEEP VARIABLE NAME
 
-dataSwNum = extract_data(data_location['simulated']['Swansea']['fullpath'],istart=LimitsNum[0],iend=LimitsNum[1])
-tNumSW = dataSwNum[:,0]
-tNumSWmiddle = dataSwNum[:,1]
-youtNumSW = dataSwNum[:,2:]
+limits_sh_num = data_particulars['simulated']['Sheffield']['slice_range']
+path_sh_num = data_particulars['simulated']['Sheffield']['fullpath']
+data_sh_num = extract_data(path_sh_num,istart=limits_sh_num[0],iend=limits_sh_num[1]) # dataShNum = extract_data(data_particulars['simulated']['Sheffield']['fullpath'],istart=LimitsNum[0],iend=LimitsNum[2])
+tNumSH = data_sh_num[:,0] # KEEP VARIABLE NAME
+tNumSHmiddle = data_sh_num[:,1]
+youtNumSH = data_sh_num[:,2:] # KEEP VARIABLE NAME
 
-dataShNum = extract_data(data_location['simulated']['Sheffield']['fullpath'],istart=LimitsNum[0],iend=LimitsNum[2])
-tNumSH = dataShNum[:,0]
-tNumSHmiddle = dataShNum[:,1]
-youtNumSH = dataShNum[:,2:]
+limits_so_num = data_particulars['simulated']['Southampton']['slice_range']
+path_so_num = data_particulars['simulated']['Southampton']['fullpath']
+data_so_num = extract_data(path_so_num,istart=limits_so_num[0],iend=limits_so_num[1])# dataSoNum = extract_data(data_particulars['simulated']['Southampton']['fullpath'],istart=LimitsNum[0],iend=LimitsNum[3])
+tNumSO = data_so_num[:,0] # KEEP VARIABLE NAME
+tNumSOmiddle = data_so_num[:,1]
+youtNumSO = data_so_num[:,1:4] # KEEP VARIABLE NAME
 
-dataSoNum = extract_data(data_location['simulated']['Southampton']['fullpath'],istart=LimitsNum[0],iend=LimitsNum[3])
-tNumSO = dataSoNum[:,0]
-tNumSOmiddle = dataSoNum[:,1]
-youtNumSO = dataSoNum[:,1:4]
-
-dataBrNum = extract_data(data_location['simulated']['Bristol']['fullpath'],istart=LimitsNum[0],iend=LimitsNum[4])
-tNumBR = dataBrNum[:,0]
-tNumBRmiddle = dataBrNum[:,1]
-youtNumBR = dataBrNum[:,1:4]
+limits_br_num = data_particulars['simulated']['Southampton']['slice_range']
+path_br_num = data_particulars['simulated']['Southampton']['fullpath']
+data_br_num = extract_data(path_br_num,istart=limits_br_num[0],iend=limits_br_num[1]) # dataBrNum = extract_data(data_particulars['simulated']['Bristol']['fullpath'],istart=LimitsNum[0],iend=LimitsNum[4])
+tNumBR = data_br_num[:,0] # KEEP VARIABLE NAME
+tNumBRmiddle = data_br_num[:,1]
+youtNumBR = data_br_num[:,1:4] # KEEP VARIABLE NAME
 
 
 # dataSwExp = readfile_gen('dtLib/crossval/Data/Experimental Data/ExpDataSw.csv')
